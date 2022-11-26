67bce766bb696dbe600a850d597283a94bcbd126
rachmatfajar <118803084+rachmatfajar@users.noreply.github.com>
2022-11-26 21:05:29 +0700

    Add files via upload

---
+++ WORLD-CLASS.CORP_RESTAURANT-KTT-G20/src/main/Main.java
@@ -0,0 +1,13 @@
+
+package main;
+
+import GUI.MenuGUI;
+
+public class Main {
+
+    public static void main(String[] args) {
+        MenuGUI tes = new MenuGUI();
+        tes.setVisible(true);
+    }
+    
+}
